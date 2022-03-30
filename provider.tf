a4237485ab866faef1045f1007e423dd1e20a108
mlogallo <85250289+mlogallo@users.noreply.github.com>
2022-03-30 11:53:34 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl/latest"
       version = ">= 1.11.3"
     }
   }
